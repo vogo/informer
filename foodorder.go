1232d5d433654fb47884d98a6feeba3e6cb463e4
wongoo <gelnyang@163.com>
2019-11-21 12:42:39 +0800

    init

---
+++ foodorder.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+type FoodType struct {
+	Type     string   `json:"type"`
+	ChoseNum int      `json:"chose_num"`
+	List     []string `json:"list"`
+}
+
+type FoodList []*FoodType
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("require ding url")
+	}
+
+	buf := bytes.NewBuffer(nil)
+
+	now := time.Now()
+	weekday := time.Now().Weekday()
+	buf.WriteString(fmt.Sprintf("今天是 %s %s\n", now.Format("2006-01-02"), weekday.String()))
+
+	if weekday == time.Sunday {
+		buf.WriteString("周末愉快!")
+	} else {
+		autoChoseFood(buf)
+	}
+
+	content := string(buf.Bytes())
+	fmt.Print(content)
+	ding(os.Args[1], content)
+}
+
+func autoChoseFood(buf *bytes.Buffer) {
+	exePath, _ := os.Executable()
+	exeDir := filepath.Dir(exePath)
+	dataPath := filepath.Join(exeDir, "foodorder.json")
+	data, err := ioutil.ReadFile(dataPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var foodList FoodList
+	if err := json.Unmarshal(data, &foodList); err != nil {
+		log.Fatal(err)
+	}
+
+	buf.WriteString("上班辛苦了! 中午为你推荐以下菜单: \n\n")
+
+	rand.Seed(time.Now().Unix())
+
+	for _, foodType := range foodList {
+		buf.WriteString(foodType.Type)
+		buf.WriteByte(':')
+		for i := 0; i < foodType.ChoseNum; i++ {
+			index := rand.Intn(len(foodType.List))
+			if i > 0 {
+				buf.WriteByte(',')
+			}
+			buf.WriteString(foodType.List[index])
+
+			foodType.List = append(foodType.List[:index], foodType.List[index+1:]...)
+		}
+		buf.WriteByte('\n')
+	}
+}
+
+type DingText struct {
+	Content string `json:"content"`
+}
+type DingMsg struct {
+	MsgType string   `json:"msgtype"`
+	Text    DingText `json:"text"`
+}
+
+func ding(url, content string) {
+	msg := &DingMsg{
+		MsgType: "text",
+		Text: DingText{
+			Content: content,
+		},
+	}
+	data, _ := json.Marshal(msg)
+	log.Printf("ding url: %s", url)
+	log.Printf("ding data: %s", data)
+	resp, err := http.Post(url, "application/json", bytes.NewReader(data))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("ding response: %v", resp)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/wongoo/foodorder
+
+go 1.13
