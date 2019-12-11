4eb58ab0b49f243d424b3de701228af48c899610
wongoo <gelnyang@163.com>
2019-12-11 10:49:02 +0800

    filter previous chosen

--- foodorder.go
+++ foodorder.go
@@ -13,13 +13,18 @@ import (
 	"time"
 )
 
-type FoodType struct {
+const (
+	configFileName         = "foodorder.json"
+	previousChosenFileName = "previous_chosen.json"
+)
+
+type FoodMenu struct {
 	Type     string   `json:"type"`
 	ChoseNum int      `json:"chose_num"`
 	List     []string `json:"list"`
 }
 
-type FoodList []*FoodType
+type FoodList []*FoodMenu
 
 func main() {
 	if len(os.Args) < 2 {
@@ -54,7 +59,7 @@ func main() {
 func autoChoseFood(buf *bytes.Buffer) {
 	exePath, _ := os.Executable()
 	exeDir := filepath.Dir(exePath)
-	dataPath := filepath.Join(exeDir, "foodorder.json")
+	dataPath := filepath.Join(exeDir, configFileName)
 	data, err := ioutil.ReadFile(dataPath)
 	if err != nil {
 		log.Fatal(err)
@@ -65,24 +70,70 @@ func autoChoseFood(buf *bytes.Buffer) {
 		log.Fatal(err)
 	}
 
+	filterPreviousChosen(exeDir, &foodList)
+
 	buf.WriteString("上班辛苦了! 中午为你推荐以下菜单: \n\n")
 
 	rand.Seed(time.Now().Unix())
 
-	for _, foodType := range foodList {
-		buf.WriteString(foodType.Type)
+	chose := make(map[string][]string)
+	for _, foodMenu := range foodList {
+		buf.WriteString(foodMenu.Type)
 		buf.WriteByte(':')
-		for i := 0; i < foodType.ChoseNum; i++ {
-			index := rand.Intn(len(foodType.List))
+		for i := 0; i < foodMenu.ChoseNum; i++ {
+			index := rand.Intn(len(foodMenu.List))
 			if i > 0 {
 				buf.WriteByte(',')
 			}
-			buf.WriteString(foodType.List[index])
+			buf.WriteString(foodMenu.List[index])
 
-			foodType.List = append(foodType.List[:index], foodType.List[index+1:]...)
+			chose[foodMenu.Type] = append(chose[foodMenu.Type], foodMenu.List[index])
+			foodMenu.List = append(foodMenu.List[:index], foodMenu.List[index+1:]...)
 		}
 		buf.WriteByte('\n')
 	}
+
+	if b, err := json.Marshal(chose); err == nil {
+		_ = ioutil.WriteFile(filepath.Join(exeDir, previousChosenFileName), b, 0660)
+	}
+}
+
+func filterPreviousChosen(exeDir string, foodList *FoodList) {
+	previousChosen := make(map[string][]string)
+	previousData, err := ioutil.ReadFile(filepath.Join(exeDir, previousChosenFileName))
+	if err != nil {
+		return
+	}
+
+	_ = json.Unmarshal(previousData, &previousChosen)
+
+	if len(previousChosen) == 0 {
+		return
+	}
+
+	log.Printf("previous chosen: %v", previousChosen)
+
+	for key := range previousChosen {
+		for _, foodMenu := range *foodList {
+			if foodMenu.Type == key {
+				filterItems(foodMenu, previousChosen[key])
+				break
+			}
+		}
+	}
+}
+
+func filterItems(foodMenu *FoodMenu, filters []string) {
+	for _, filter := range filters {
+		for index, name := range foodMenu.List {
+			if name == filter {
+				foodMenu.List = append(foodMenu.List[:index], foodMenu.List[index+1:]...)
+				break
+			}
+		}
+	}
+
+	log.Printf("after filter for %s: %s", foodMenu.Type, foodMenu.List)
 }
 
 type DingText struct {
